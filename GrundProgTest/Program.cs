87fb2f8aecd19bde0557b31a8d6f64846f1c16e8
tristanlifn <tristanlif@protonmail.com>
2026-06-08 08:53:48 +0200

    added 'FindBorrower' method to find a specific borrower

--- GrundProgTest/Program.cs
+++ GrundProgTest/Program.cs
@@ -11,20 +11,22 @@ class Program
 
 class Library
 {
+    private List<Borrower> _borrowers;
+    
     public void Main()
     {
-        List<Borrower> borrowers =
+        _borrowers =
         [
             new(1, "Kolding bibliotek", "Henrik", "henrik@gmail.com", "+4541267412"),
             new(2, "Sønderborg bibliotek", "Denis", "denis@gmail.com", "+4553152235"),
             new(3, "Kolding bibliotek", "Nikolaj", "nikolaj@gmail.com", "+4572373521")
         ];
 
-        PrintBorrowers(borrowers);
+        PrintBorrowers(_borrowers);
     }
 
     /// <summary>
-    /// Print all the borrowers via the 'GetBorrower' mwthon in the Borrower class
+    /// Print all the borrowers via the 'GetBorrower' method in the Borrower class
     /// </summary>
     /// <param name="borrowers">A list of Borrowers to print</param>
     private static void PrintBorrowers(List<Borrower> borrowers)
@@ -33,5 +35,20 @@ class Library
         {
             Console.WriteLine(borrower.GetBorrower());
         }
+    }
+
+    /// <summary>
+    /// Finds a borrower in the '_borrowers' list, if one exists, writes the info, else writes that no borrower was found
+    /// </summary>
+    /// <param name="borrowerNum">the borrower number to search for</param>
+    private void FindBorrower(int borrowerNum)
+    {
+        Borrower? foundBorrower = _borrowers.FirstOrDefault(x => x.BorrowerNum == borrowerNum);
+        
+        if (foundBorrower == null)
+            Console.WriteLine("No borrower found with borrower num: " + borrowerNum);
+        else
+            Console.WriteLine(foundBorrower.GetBorrower());
+        
     }
 }
